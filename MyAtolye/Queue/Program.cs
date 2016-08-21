6c47ed727725bb9e417016ea3e778f7bf975f457
theWornOut <ekremalpayy@gmail.com>
2016-08-21 16:13:35 +0300

    Stack-Queue

---
+++ MyAtolye/Queue/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> queue = new Queue<string>();
+            queue.Enqueue("Ekrem");
+            queue.Enqueue("Alpay");
+            queue.Enqueue("24");
+            queue.Enqueue("77");
+
+            Console.WriteLine("Çıkan eleman {0}", queue.Dequeue().ToString());
+            Console.WriteLine("Çıkan eleman {0}", queue.Dequeue().ToString());
+            Console.WriteLine("Çıkan eleman {0}", queue.Dequeue().ToString());
+            Console.WriteLine("------------------");
+
+            IEnumerator dizi = queue.GetEnumerator();
+            while (dizi.MoveNext())
+            {
+                Console.WriteLine("Güncel eleman {0}", dizi.Current.ToString());
+            }
+            Console.WriteLine("------------------");
+            Console.WriteLine("En altta kalan eleman {0}", queue.Peek());
+            dizi = queue.GetEnumerator();
+            while (dizi.MoveNext())
+            {
+                Console.WriteLine("Güncel eleman {0}", dizi.Current.ToString());
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ MyAtolye/StackQueue/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack(4);
+            stack.Push("Ekrem");
+            stack.Push("Alpay");
+            stack.Push("24");
+            stack.Push("77");
+            stack.Push(true);
+
+            Console.WriteLine($"Çıkan eleman {stack.Pop().ToString()}");
+            Console.WriteLine($"Çıkan eleman {stack.Pop().ToString()}");
+            Console.WriteLine($"Çıkan eleman {stack.Pop().ToString()}");
+            Console.WriteLine("-------------");
+
+            IEnumerator dizi = stack.GetEnumerator();
+            while (dizi.MoveNext())
+            {
+                Console.WriteLine($"Guncel eleman {dizi.Current.ToString()}");
+            }
+            Console.WriteLine("-------------");
+            Console.WriteLine($"En uzun eleman {stack.Peek()}");
+            dizi = stack.GetEnumerator();
+            while (dizi.MoveNext())
+            {
+                Console.WriteLine($"Guncel eleman {dizi.Current.ToString()}");
+            }
+        }
+    }
+}
