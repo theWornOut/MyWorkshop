fb914157f8ac2f83b43f21c7b84f432e93ba7b60
theWornOut <ekremalpayy@gmail.com>
2016-08-22 15:53:28 +0300

    Delegate

---
+++ MyAtolye/DelegateExp/MyDelegates.cs
@@ -0,0 +1,13 @@
+﻿namespace DelegateExp
+{
+    public class MyDelegates
+    {
+        public static int myDelegateInt;
+        public delegate void temsilci(int deger);
+
+        public void myDelegateMethod(temsilci deger)
+        {
+            deger(myDelegateInt);
+        }
+    }
+}
---
+++ MyAtolye/DelegateExp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using static DelegateExp.MyDelegates;
+
+namespace DelegateExp
+{
+    class Program
+    {
+        public static void CiftKat(int sayi)
+        {
+            sayi = sayi * 2;
+            Console.WriteLine($"CiftKat isimli methodun temsilcisi tarafından cagrildi. {sayi.ToString()}");
+        }
+
+        public static void UcKat(int sayi)
+        {
+            sayi = sayi * 3;
+            Console.WriteLine($"UcKat isimli methodun temsilcisi tarafından cagrildi. {sayi.ToString()}");
+        }
+
+        static void Main(string[] args)
+        {
+            temsilci t1 = new temsilci(CiftKat);
+            temsilci t2 = new temsilci(UcKat);
+
+            Random rnd = new Random();
+            myDelegateInt = rnd.Next(0, 100);
+
+            MyDelegates myDelegates = new MyDelegates();
+            if (myDelegateInt > 50)
+            {
+                myDelegates.myDelegateMethod(t1);
+            }
+            else
+            {
+                myDelegates.myDelegateMethod(t2);
+            }
+        }
+    }
+}
