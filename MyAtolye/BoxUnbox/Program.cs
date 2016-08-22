510ef9724fe90ca2fbbaac1533783eda8cdb84c0
theWornOut <ekremalpayy@gmail.com>
2016-08-22 11:17:59 +0300

    Box-Unbox / Cast-Uncast

---
+++ MyAtolye/BoxUnbox/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BoxUnbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double db = 509809232323;
+            object obj;
+            obj = db;
+            Console.WriteLine(db.ToString());
+            Console.WriteLine(obj.ToString());
+            db += 1;
+            Console.WriteLine(db.ToString());
+            Console.WriteLine(obj.ToString());
+            /*int intDb;
+            intDb=(int)obj;
+            Console.WriteLine(intDb.ToString());*/
+            double dobDb;
+            dobDb = (double)obj;
+            Console.WriteLine(dobDb.ToString());
+        }
+    }
+}
