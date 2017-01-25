e9f2a2dd036339a67752e237db890a56f6cac56c
theWornOut <ekremalpayy@gmail.com>
2017-01-25 20:35:26 +0300

    Enum use to or

---
+++ MyAtolye/EnumOr/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EnumOr
+{
+    class Program
+    {
+        [Flags]
+        enum Security
+        {
+            none = 0, read = 1, write = 2
+        }
+
+        static void Main(string[] args)
+        {
+            Security s = Security.read | Security.write;
+            Console.WriteLine(s);
+            Console.WriteLine((int)s);
+
+            Console.ReadKey();
+        }
+    }
+}
